1cb8c121c024b7598167b8a2000f97381abdd094
Salih Değirmenci <salihdegirmenci99@gmail.com>
2021-03-13 16:15:16 +0300

    Project Creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarComponent } from './components/car/car.component';
+import { BrandComponent } from './components/brand/brand.component';
+import { ColorComponent } from './components/color/color.component';
+import { CustomerComponent } from './components/customer/customer.component';
+import { NaviComponent } from './components/navi/navi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarComponent,
+    BrandComponent,
+    ColorComponent,
+    CustomerComponent,
+    NaviComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/models/car.ts
@@ -0,0 +1,9 @@
+export interface Car {
+  carId: number;
+  brandId: number;
+  colorId: number;
+  modelYear: string;
+  dailyPrice: number;
+  carDescription: string;
+  isAvailable: boolean;
+}
